1ff3304fa95a198a4267e006d2b8c790e9fa50b3
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-12 22:57:24 +0530

    todo assignment completed v1

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import { useTodo } from '../contexts/TodoContext';
 
 function TodoForm() {
 	const [todo, setTodo] = useState('');
 
+	const { addTodo } = useTodo();
+
+	const add = e => {
+		e.preventDefault();
+		if (!todo) return;
+		addTodo({ todo, completed: false });
+		setTodo('');
+	};
 
 	return (
-		<form className="flex" >
+		<form className="flex" onSubmit={add}>
 			<input
 				type="text"
 				value={todo}
